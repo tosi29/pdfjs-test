91ac116a286bccfefe09d5cb2de50bc50b450ba0
tosi29 <tosi16@gmail.com>
2025-03-01 20:26:10 +0900

    PDFを表示できるようにした

--- src/App.js
+++ src/App.js
@@ -1,22 +1,41 @@
-import logo from './logo.svg';
+import React, { useEffect, useRef } from 'react';
+import * as pdfjsLib from 'pdfjs-dist/build/pdf';
+import 'pdfjs-dist/build/pdf.worker';
 import './App.css';
 
 function App() {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const url = 'https://mozilla.github.io/pdf.js/web/compressed.tracemonkey-pldi-09.pdf';
+
+    async function loadPdf() {
+      pdfjsLib.GlobalWorkerOptions.workerSrc = `//mozilla.github.io/pdf.js/build/pdf.worker.js`;
+
+      const pdfDoc = await pdfjsLib.getDocument(url).promise;
+      const pdfPage = await pdfDoc.getPage(1);
+      const viewport = pdfPage.getViewport({ scale: 1 });
+
+      const canvas = canvasRef.current;
+      const canvasContext = canvas.getContext('2d');
+      canvas.height = viewport.height;
+      canvas.width = viewport.width;
+
+      const renderContext = {
+        canvasContext,
+        viewport,
+      };
+      pdfPage.render(renderContext);
+    }
+
+    loadPdf();
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>PDF.js Sample</h1>
+        <canvas ref={canvasRef} />
       </header>
     </div>
   );
